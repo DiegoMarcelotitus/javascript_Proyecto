df1455bf30426a1e4bcd2b0fb38da94ef0866e7d
DiegoMarcelotitus <diegotitus@hotmail.com>
2023-06-10 17:42:08 -0300

    update-javascript

--- javascrip.js
+++ javascrip.js
@@ -1,52 +1,106 @@
-//inicio de calculo enteres y cuotas
+inicio_sistema();
+eventos_sistema()
 
-function calcula_interes(monto, cuotas) {
 
-    if (cuotas == 1) {
-      let interes = monto * 0.05;
-      return interes;
-    }
-    else if (cuotas == 3) {
-      let interes = monto * 0.18;
-      return interes;
-    }
-    else if (cuotas == 6) {
-      let interes = monto * 0.45;
-      return interes;
-    }
-    else if (cuotas == 9) {
-      let interes = monto * 0.85;
-      return interes;
-    }
-    else if (cuotas == 12) {
-      let interes = monto * 1;
-      return interes;
-    }
-  }
-  
-  console.log("Bienvenido/a Prestamos ELgarca");
-  
-  let monto = 0;
-  
-  //inicio de bucle 
+class Prestamo{
 
-  while (monto == 0) {
-    monto = parseInt(prompt("Ingrese el monto a solicitar y para salir el número 0: "));
+    constructor( monto,cuotas,nombre){
+
+        this.nombre = nombre;
+        this.monto = Number.parseInt(monto);
+        this.cuotas = cuotas;
+        this.monto_final = 0; 
+
+    }
+
+
+    calcular_cuotas(){
+        
+        let interes = 0;
+
+        if(this.cuotas == 1 ){
+           interes= this.monto * 0.10;
+            this.monto_final = this.monto  + interes;
+        }
+        else if( this.cuotas == 3){
+           interes= this.monto * 0.35;
+            this.monto_final = this.monto / 3 + interes;
+        }
+        else if( this.cuotas == 6){
+            interes=this.monto * 0.60;
+            this.monto_final = this.monto /6 + interes;
+        }
+        else if( this.cuotas == 12){
+           interes= this.monto * 100;
+            this.monto_final = this.monto /12 + interes;
+            
+        }
+
+    }
+
     
-    if (monto == 0) {
-      console.log("Saliendo...");
-      break;
+    get_datos(){
+
+        console.log("<-- Prestamo titus -->");
+        console.log("Titular: ", this.nombre);
+        console.log("Monto: ", this.monto);
+        console.log("Cuotas: ", this.cuotas);
+        console.log("Pagarias en : ",this.cuotas,"cuotas de ", this.monto_final);
+        console.log("");
+
     }
-  
-    let cuotas = parseInt(prompt("Ingrese el número de cuotas (1, 3, 6, 9, 12): "));
-    let interes = calcula_interes(monto, cuotas);
-    let total = monto + interes;
-  
-    console.log("Prestamo pedido: ", monto);
-    console.log("En cantidad de cuotas:", cuotas);
-    console.log("Su total con interés:", total);
-    console.log("Pagas en cuotas:", total / cuotas);
+    get_resultado(){
+        return this.monto_final;
+    }
+    
+
+}
+
+
+function filtrar_prestamos( prestamo){
+    
+    return prestamo.nombre == nombre
+}
+///aca renderiza los elementos de pantalla
+function inicio_sistema(){
+    document.getElementById('app').innerHTML+= `<h1>Bienvenidos Prestamos Titus</h1>`;
+    document.getElementById('app').innerHTML+= `<p>Ingresar un Monto</p>`;
+    document.getElementById('app').innerHTML+= `<input id="Monto">`;
+    document.getElementById('app').innerHTML+= `<p>Ingresar Cantidad de Cuota</p>`;
+    document.getElementById('app').innerHTML+= `<input id="Cuotas">`;
+    document.getElementById('app').innerHTML+='<p>Ingrese su Nombre</p>';
+    document.getElementById('app').innerHTML+= `<input id="Nombre">`;
+    document.getElementById('app').innerHTML+= `<p>Monto total</p>`;
+    document.getElementById('app').innerHTML+= `<input  placeholder="INGRESE UN VALORES PARA MOSTRAR UN RESULTADO OK" disabled=true id="monto_final">`;
+
+    document.getElementById('app').innerHTML+='<button  id="btn"> Calcular </button>';
+
+    
+ }
+ function eventos_sistema(){
+
+    document.getElementById('btn').addEventListener('click',()=>{
+     // traigo el valor de el html 
+    let monto_del_html= document.getElementById("Monto").value;
+    let cuotas_del_html= document.getElementById("Cuotas").value;
+    let nombre_del_html= document.getElementById("Nombre").value;
+    let monto_final_html= document.getElementById("monto_final").value;
+     if((!Number.parseInt(cuotas_del_html)>0)&& (!Number.parseInt(monto_del_html>0))){ 
+
+        alert('no pusiste monto');
+        alert('no pusiste cantidad de cuotas');
+        return;
+    
+     }
+    
+      let programa = new Prestamo(monto_del_html,cuotas_del_html,nombre_del_html,monto_final_html); 
+      programa.calcular_cuotas();
+      programa.get_datos();
+        document.getElementById("monto_final").value=programa.get_resultado();
+    });
   }
+
+
 
 
 
